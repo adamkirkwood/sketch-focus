2a3ad5ef387061becef0ebc0c891ad70d67b87da
Lukas Ondrej <lukas77me@gmail.com>
2016-07-25 09:07:32 +0100

    update to 1.0.2

--- SketchFocus.sketchplugin/Contents/Resources/SketchFocus.framework/Versions/A/Resources/SketchFocusHelper.app/Contents/Resources/script.js
+++ SketchFocus.sketchplugin/Contents/Resources/SketchFocus.framework/Versions/A/Resources/SketchFocusHelper.app/Contents/Resources/script.js
@@ -1,10 +1,18 @@
 var codeToRun = "\
-if(!loadFramework()) return;\
+if(!loadFramework()) {\
+    if(!loadFramework(true)) return;\
+}\
 PLUGIN_NAME.loadWindow();\
-function loadFramework() {\
+function loadFramework(inFolder) {\
     if(NSClassFromString('PLUGIN_NAME') != null) return true;\
     var appSupportPath = NSFileManager.defaultManager().URLsForDirectory_inDomains(NSApplicationSupportDirectory,NSUserDomainMask).firstObject().path();\
-    var pluginFolderPath = appSupportPath.stringByAppendingPathComponent('com.bohemiancoding.sketch3/Plugins/PLUGIN_NAME.sketchplugin');\
+    var pluginFolderPath;\
+    if(inFolder) {\
+        pluginFolderPath = appSupportPath.stringByAppendingPathComponent('com.bohemiancoding.sketch3/Plugins/PLUGIN_FOLDER_NAME/PLUGIN_NAME.sketchplugin');\
+    }\
+    else {\
+        pluginFolderPath = appSupportPath.stringByAppendingPathComponent('com.bohemiancoding.sketch3/Plugins/PLUGIN_NAME.sketchplugin');\
+    }\
     var resourcesPath = pluginFolderPath.stringByAppendingPathComponent('Contents/Resources');\
     var mocha = Mocha.sharedRuntime();\
     return mocha.loadFrameworkWithName_inDirectory('PLUGIN_NAME',resourcesPath);\
